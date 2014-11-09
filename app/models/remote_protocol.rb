0f8d457fd57ebee9df52eb8dcf9aa8494db94e4c
Oleg Antonyan <oleg.b.antonyan@gmail.com>
2014-11-09 12:42:27 +0200

    Updated rails

--- app/models/remote_protocol.rb
+++ app/models/remote_protocol.rb
@@ -16,27 +16,20 @@ class RemoteProtocol
   def process_incoming(from, msg, online=true)
     puts "#{from} is #{online ? 'online' : 'offline'} : '#{msg}'"
     
-    d = Device.find_by(:login => from)
-    unless d.nil?
-      unless d.device_status.nil?
-        d.device_status.update_attributes(:online => online, :now_playing => msg)
-      else
-        d.device_status = DeviceStatus.new
-        d.device_status.online = online
-        d.device_status.now_playing = msg
-        d.device_status.save
+    if msg.start_with?("{")
+      data = JSON.parse(msg)
+      case data["type"]
+        when "power" 
+          update_device(from, online, "power_on")
+        when "playback" 
+          update_device(from, online, data["now_playing"])
+        else 
+          update_device(from, online, "unknown")
       end
     else
-      d = Device.new
-      d.login = from
-      d.serial_number = serial_number_from_login d.login
-      d.name = "auto added device #{d.serial_number}"
-      d.device_status = DeviceStatus.new
-      d.device_status.online = online
-      d.device_status.now_playing = msg
-      d.save
+      update_device(from, online, msg)
     end
-    
+           
   end
   
   def server_online?
@@ -55,6 +48,32 @@ class RemoteProtocol
   
   def json_for_delete_playlist
     {'type' => 'playlist', 'status' => 'delete'}.to_json
+  end
+  
+  private
+  
+  def update_device(login, online, now_playing)
+    
+    d = Device.find_by(:login => login)
+    unless d.nil?
+      unless d.device_status.nil?
+        d.device_status.update_attributes(:online => online, :now_playing => now_playing)
+      else
+        d.device_status = DeviceStatus.new
+        d.device_status.online = online
+        d.device_status.now_playing = now_playing
+        d.device_status.save
+      end
+    else
+      d = Device.new
+      d.login = login
+      d.serial_number = serial_number_from_login d.login
+      d.name = "auto added device #{d.serial_number}"
+      d.device_status = DeviceStatus.new
+      d.device_status.online = online
+      d.device_status.now_playing = now_playing
+      d.save
+    end
   end
   
 end
