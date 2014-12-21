8a49ae587d9ca61f7c532fdb8b14b0cc5955f632
Oleg Antonyan <oleg.b.antonyan@gmail.com>
2014-12-21 21:47:35 +0200

    Minor refactro

--- config/initializers/scheduler.rb
+++ config/initializers/scheduler.rb
@@ -1,6 +1,12 @@
-require 'rufus-scheduler'
+if defined?(Rails::Console) or defined?(Rails::Generators) or File.basename($0) == "rake"
+  # skip console, rails generators and rake
+else
+  require 'rufus-scheduler'
 
-scheduler = Rufus::Scheduler.new
-scheduler.every '10s' do
-  Deviceapi::Timeouts.check
+  scheduler = Rufus::Scheduler.new
+  
+  scheduler.every '10s' do
+    Deviceapi::Timeouts.check
+  end
+  
 end
