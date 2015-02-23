c1ab5f31a9711d600c8ba8058a5c150e7ea0d12f
Oleg Antonyan <oleg.b.antonyan@gmail.com>
2015-02-23 14:47:31 +0200

    Locale from domain

--- app/controllers/users_application_controller.rb
+++ app/controllers/users_application_controller.rb
@@ -6,11 +6,23 @@ class UsersApplicationController < ApplicationController
   before_action :set_locale
 
   def set_locale
-    I18n.locale = params[:lang] || I18n.default_locale
+    #I18n.locale = params[:lang] || I18n.default_locale
+    I18n.locale = extract_locale_from_tld || I18n.default_locale
   end
   
-  def default_url_options(options = {})
-    { lang: I18n.locale }.merge options
+  #def default_url_options(options = {})
+  #  { lang: I18n.locale }.merge options
+  #end
+  
+  # Get locale from top-level domain or return nil if such locale is not available
+  # You have to put something like:
+  #   127.0.0.1 application.com
+  #   127.0.0.1 application.it
+  #   127.0.0.1 application.pl
+  # in your /etc/hosts file to try this out locally
+  def extract_locale_from_tld
+    parsed_locale = request.host.split('.').last
+    I18n.available_locales.map(&:to_s).include?(parsed_locale) ? parsed_locale : nil
   end
   
   def flash_error(msg)
