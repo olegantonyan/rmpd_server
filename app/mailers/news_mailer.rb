b30ecf4faf66a588118745af55de23a09386c28d
Oleg Antonyan <oleg.b.antonyan@gmail.com>
2015-05-09 15:56:47 +0200

    Fix date format in news mailer

--- app/mailers/news_mailer.rb
+++ app/mailers/news_mailer.rb
@@ -9,7 +9,7 @@ class NewsMailer < ApplicationMailer
       @unsibscribe_url =  edit_user_registration_url
       @signature = t(:signature, :app_title => app_title)
       @greetings = "#{t(:greetings)}, #{user.displayed_name.blank? ? t(:dear_customer) : user.displayed_name}, #{t(:news_title)}"
-      mail(to: user.email, subject: t(:subject, :app_title => app_title, :title => sanitize(news_item.title), :date => news_item.created_at.strftime('%m.%d.%Y')))
+      mail(to: user.email, subject: t(:subject, :app_title => app_title, :title => sanitize(news_item.title), :date => news_item.created_at.strftime('%d.%m.%Y')))
     end
   end
   
