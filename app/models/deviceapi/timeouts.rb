effd7fae6cc96d4bbf7a8be1e541b813a8e2c609
Oleg Antonyan <oleg.b.antonyan@gmail.com>
2015-05-10 12:26:02 +0200

    Device offline timeout 45 seconds

--- app/models/deviceapi/timeouts.rb
+++ app/models/deviceapi/timeouts.rb
@@ -1,6 +1,6 @@
 class Deviceapi::Timeouts
   def self.check
-    devices = DeviceStatus.where("online = ? AND updated_at <= ?", true, Time.now - 30)
+    devices = DeviceStatus.where("online = ? AND updated_at <= ?", true, Time.now - 45)
     count = devices.size
     if count > 0
       devices.update_all(:online => false, :now_playing => "")
