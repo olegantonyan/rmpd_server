e07fa18c7c465eb89c9889a01f5e0b6eed4796d2
Oleg Antonyan <oleg.b.antonyan@gmail.com>
2015-06-26 06:41:08 +0200

    Fix create playlist bug #41

--- app/models/playlist.rb
+++ app/models/playlist.rb
@@ -5,34 +5,34 @@ class Playlist < ActiveRecord::Base
   has_many :media_deployments, -> {
     order(:playlist_position)
     }, dependent: :destroy, autosave: true
-  has_many :media_items, -> { 
-    joins(:media_deployments).order('media_deployments.playlist_position').group('media_items.id, media_deployments.playlist_position') 
+  has_many :media_items, -> {
+    joins(:media_deployments).order('media_deployments.playlist_position').group('media_items.id, media_deployments.playlist_position')
     }, through: :media_deployments, autosave: true
-    
+
   has_many :devices
   belongs_to :company
-  
+
   after_save :playlist_updated
   after_destroy :playlist_destroyed
-  
+
   mount_uploader :file, PlaylistFileUploader
-  
+
   validates_presence_of :name
   validates_length_of :name, :maximum => 130
   validates_length_of :description, :maximum => 250
   validates_presence_of :media_deployments
   #validates_presence_of :media_items
   validate :check_files_processing
-  
+
   def deploy_media_items!(items, media_items_positions)
     media_deployments.destroy_all
     items.each do |i|
-      playlist_position = media_items_positions.find{ |e| e.first.to_i == i.id}.second 
-      media_deployments << MediaDeployment.create(:media_item => i, :playlist_position => playlist_position)
+      playlist_position = media_items_positions.find{ |e| e.first.to_i == i.id}.second
+      media_deployments << MediaDeployment.create(:media_item => i, :playlist_position => playlist_position, :playlist => self)
     end
   end
-  
-  rails_admin do 
+
+  rails_admin do
     list do
       field :name
       field :description
@@ -47,11 +47,11 @@ class Playlist < ActiveRecord::Base
       exclude_fields :media_deployments, :versions, :file
     end
   end
-  
+
   def to_s
     (description.blank? ? "#{name}" : "#{name} (#{description})") + " in #{company.to_s}"
   end
-  
+
   private
     def create_playlist_file
       tempfile = Tempfile.new(['playlist', '.m3u'])
@@ -62,31 +62,31 @@ class Playlist < ActiveRecord::Base
       self.file = tempfile
       tempfile.unlink
     end
-    
+
     def playlist_updated
       create_playlist_file
-      Playlist.skip_callback(:save, :after, :playlist_updated) # skipping callback is required to prevent recursion 
+      Playlist.skip_callback(:save, :after, :playlist_updated) # skipping callback is required to prevent recursion
       save  # save newly created file in db
-      Playlist.set_callback(:save, :after, :playlist_updated) 
-      
+      Playlist.set_callback(:save, :after, :playlist_updated)
+
       self.devices.each do |d|
         Deviceapi::Protocol.new.update_playlist d
       end
     end
-    
+
     def playlist_destroyed
       self.devices.each do |d|
         Deviceapi::Protocol.new.delete_playlist d
       end
     end
-    
+
     def check_files_processing
       if media_deployments.find{|i| !i.valid? }
         errors.add(:base, I18n.t(:files_processing)) # for error message only
         false
       else
-        true    
+        true
       end
     end
-  
+
 end
