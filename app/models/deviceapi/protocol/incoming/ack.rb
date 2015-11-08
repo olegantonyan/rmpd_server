bedf25a1fc4babd2a8f22a6d08776dd41d905598
Oleg Antonyan <oleg.b.antonyan@gmail.com>
2015-11-08 11:07:55 +0200

    Minor refactor in ack messae

--- app/models/deviceapi/protocol/incoming/ack.rb
+++ app/models/deviceapi/protocol/incoming/ack.rb
@@ -1,14 +1,15 @@
 class Deviceapi::Protocol::Incoming::Ack < Deviceapi::Protocol::Incoming::BaseCommand
   def call(device, data, options = {})
     sequence_number = options.fetch(:sequence_number)
+    mq = Deviceapi::MessageQueue
     if data[:status] == 'ok'
-      Deviceapi::MessageQueue.remove(sequence_number)
+      mq.remove(sequence_number)
     else
-      if Deviceapi::MessageQueue.retries(sequence_number) < 15
-        Deviceapi::MessageQueue.reenqueue(sequence_number)
+      if mq.retries(sequence_number) < 15
+        mq.reenqueue(sequence_number)
       else
         Rails.logger.warn("Maximum retries reached for device '#{device.login}'")
-        Deviceapi::MessageQueue.remove(sequence_number)
+        mq.remove(sequence_number)
       end
     end
   end
