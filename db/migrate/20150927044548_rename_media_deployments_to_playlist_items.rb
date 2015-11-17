13a02467b75cb520732599fefac44d9a00fbe751
Oleg Antonyan <oleg.b.antonyan@gmail.com>
2015-11-17 06:05:09 +0200

    Fix failing migration

--- db/migrate/20150927044548_rename_media_deployments_to_playlist_items.rb
+++ db/migrate/20150927044548_rename_media_deployments_to_playlist_items.rb
@@ -1,6 +1,6 @@
 class RenameMediaDeploymentsToPlaylistItems < ActiveRecord::Migration
   def change
-    rename_table :playlist_items, :playlist_items
+    rename_table :media_deployments, :playlist_items
     rename_column :playlist_items, :playlist_position, :position
   end
 end
