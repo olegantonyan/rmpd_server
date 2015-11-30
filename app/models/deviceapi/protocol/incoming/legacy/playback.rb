5cacd538858436afe3e3042be123bc476bdd7355
Oleg Antonyan <oleg.b.antonyan@gmail.com>
2015-11-30 06:53:42 +0200

    Fix legacy now_playing and removed unnecessory update_plylist on none

--- app/models/deviceapi/protocol/incoming/legacy/playback.rb
+++ app/models/deviceapi/protocol/incoming/legacy/playback.rb
@@ -1,7 +1,7 @@
 class Deviceapi::Protocol::Incoming::Legacy::Playback < Deviceapi::Protocol::Incoming::BaseCommand
   def call(options = {})
     if data[:status] == 'now_playing'
-      Deviceapi::Protocol::Incoming::NowPlaying.new(device, data, sequence_number).call
+      Deviceapi::Protocol::Incoming::NowPlaying.new(device, data.merge(message: data[:track]), sequence_number).call
     end
   end
 end
--- app/models/deviceapi/protocol/incoming/now_playing.rb
+++ app/models/deviceapi/protocol/incoming/now_playing.rb
@@ -1,10 +1,5 @@
 class Deviceapi::Protocol::Incoming::NowPlaying < Deviceapi::Protocol::Incoming::BaseCommand
-  include Deviceapi::Sender
-
   def call(options = {})
     device.device_status.now_playing = data[:message]
-    if %w(none nothing).include? data[:message]
-      send_to_device(:update_playlist, device)
-    end
   end
 end
