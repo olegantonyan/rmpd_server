6647458f66d42750bfeb11f74139f9cba7f617be
Oleg Antonyan <oleg.b.antonyan@gmail.com>
2016-01-12 11:37:11 +0200

    Optimize shcedule algorithm

--- app/services/schedule/scheduler.rb
+++ app/services/schedule/scheduler.rb
@@ -3,7 +3,7 @@ class Schedule::Scheduler
 
   def initialize(itms)
     self.items = itms.select(&:advertising?).map { |i| Schedule::Item.new(i) }
-    1.upto(items.map(&:schedule_times).flatten.size * 2) do
+    1.upto(items.size**2) do
       optimize
       break unless overlap
     end
