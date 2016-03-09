2a7402b72475af153b143293dc31c07f9ef69a67
Oleg Antonyan <oleg.b.antonyan@gmail.com>
2016-03-09 15:43:25 +0200

    Fix update timezone

--- app/services/deviceapi/protocol/outgoing/update_setting.rb
+++ app/services/deviceapi/protocol/outgoing/update_setting.rb
@@ -1,15 +1,15 @@
 class Deviceapi::Protocol::Outgoing::UpdateSetting < Deviceapi::Protocol::Outgoing::BaseCommand
-  def call(*options)
+  def call(options = {})
     return unless device
     clean_previous_commands
-    enqueue(json(*options))
+    enqueue(json(options))
   end
 
   private
 
-  def json(*options)
+  def json(options = {})
     result = {}
-    result[:time_zone] = device.time_zone_formatted_offset if options.include?(:time_zone)
+    result[:time_zone] = device.time_zone_formatted_offset if options.fetch(:changed_attrs, []).include?(:time_zone)
     result
   end
 end
