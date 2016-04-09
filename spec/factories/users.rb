3d93d34f3995da67969e619e9ec974cab7d8899c
Oleg Antonyan <oleg.b.antonyan@gmail.com>
2016-04-09 06:50:29 +0200

    policy specs started

--- spec/factories/users.rb
+++ spec/factories/users.rb
@@ -2,5 +2,9 @@ FactoryGirl.define do
   factory :user do
     email { Faker::Internet.email }
     password { Faker::Internet.password(8) }
+
+    factory :user_root do
+      root { true }
+    end
   end
 end
---
+++ spec/policies/device_policy_spec.rb
@@ -0,0 +1,48 @@
+require 'rails_helper'
+
+RSpec.describe DevicePolicy, type: :policy do
+  subject { described_class.new(user, object) }
+
+  context 'for root' do
+    let(:object) { create(:device) }
+    let(:user) { create(:user_root) }
+
+    pundit_permit(*%i(index show new edit create update destroy))
+  end
+
+  context 'for member of the company' do
+    let(:object) { create(:device) }
+    let(:user) { create(:user, companies: [object.company]) }
+
+    pundit_permit(*%i(index show new edit create update destroy))
+  end
+
+  context 'for everyone else' do
+    let(:object) { create(:device) }
+    let(:user) { create(:user) }
+
+    pundit_forbid(*%i(edit update destroy show))
+    pundit_permit(*%i(index create new))
+  end
+
+  describe 'scope' do
+    before :each do
+      5.times { create(:device, company: company_1) }
+      5.times { create(:device, company: company_2) }
+    end
+
+    let(:company_1) { create(:company) }
+    let(:company_2) { create(:company) }
+    let(:object_class) { Device }
+
+    it 'returns all records to root' do
+      expect(object_class.all).not_to be_empty
+      pundit_policy_scope(create(:user_root), object_class.all, object_class.all)
+    end
+
+    it 'returns only belonging records to company' do
+      pundit_policy_scope(create(:user, companies: [company_1]), object_class.all, object_class.where(company: company_1))
+      pundit_policy_scope(create(:user, companies: [company_2]), object_class.all, object_class.where(company: company_2))
+    end
+  end
+end
