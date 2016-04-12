7068136880841bbbf8fa8d4eba83a7f729567ebd
Oleg Antonyan <oleg.b.antonyan@gmail.com>
2016-04-12 20:10:03 +0200

    protect_from_forgery try-fix

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,7 +1,8 @@
 class ApplicationController < ActionController::Base
+  before_action :authenticate_user!
   # Prevent CSRF attacks by raising an exception.
   # For APIs, you may want to use :null_session instead.
-  protect_from_forgery with: :exception, prepend: true
+  protect_from_forgery with: :exception
 
   before_action :set_locale
   before_action :configure_permitted_parameters, if: :devise_controller?
