d1f3c76a790bedbf7bda0db96e361a4b302a1a13
Oleg Antonyan <oleg.b.antonyan@gmail.com>
2016-04-12 12:55:22 +0200

    skip before action auth token

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,11 +1,13 @@
 class ApplicationController < ActionController::Base
   # Prevent CSRF attacks by raising an exception.
   # For APIs, you may want to use :null_session instead.
-  protect_from_forgery with: :null_session
+  protect_from_forgery with: :exception
 
   before_action :set_locale
   before_action :configure_permitted_parameters, if: :devise_controller?
   before_action :set_paper_trail_whodunnit
+
+  skip_before_action :verify_authenticity_token, if: :devise_controller?, only: :create
 
   private
 
