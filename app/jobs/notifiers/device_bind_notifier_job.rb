c12bfc4c1ad2169de47d9db4fe090e9ab411c8d0
Oleg Antonyan <oleg.b.antonyan@gmail.com>
2016-05-16 18:35:22 +0200

    Notify on bound device

---
+++ app/jobs/notifiers/device_bind_notifier_job.rb
@@ -0,0 +1,14 @@
+class Notifiers::DeviceBindNotifierJob < Notifiers::BaseNotifierJob
+  def perform(device)
+    text = "Device #{device} bound to #{device.company}"
+    a = {
+      fields: [{ title: 'Device login', value: device.login, short: true },
+               { title: 'Company', value: device.company.to_s, short: true }],
+      color: 'good',
+      fallback: text,
+      author_name: device.to_s,
+      author_link: device_url(device)
+    }
+    notify(text, icon_emoji: ':link:', attachments: [a])
+  end
+end
