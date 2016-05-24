608089b7724f06a94648ace047b7d98086162bb7
Oleg Antonyan <oleg.b.antonyan@gmail.com>
2016-05-24 12:44:40 +0200

    dont preocess files without audio

--- lib/mediafiles_utils.rb
+++ lib/mediafiles_utils.rb
@@ -11,7 +11,8 @@ module MediafilesUtils
   end
 
   def normalize_volume(file)
-    max_volume = `ffmpeg -i '#{file}' -af "volumedetect" -f null /dev/null 2>&1 | grep max_volume`.strip.split(':').last.strip.split(' ').first.to_f
+    max_volume = `ffmpeg -i '#{file}' -af "volumedetect" -f null /dev/null 2>&1 | grep max_volume`.strip&.split(':')&.last&.strip&.split(' ')&.first&.to_f
+    return unless max_volume
     return if max_volume.zero?
     output_file = "/tmp/#{File.basename(file)}"
     result = system(*['ffmpeg', '-i', file, '-af', "volume=#{-max_volume}dB", '-c:v', 'copy', output_file])
