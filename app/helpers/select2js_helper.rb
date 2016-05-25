d016cde78876a865fb4945e4124dc4306a58e007
Oleg Antonyan <oleg.b.antonyan@gmail.com>
2016-05-25 06:05:06 +0200

    enable turbolinks cache

--- app/helpers/select2js_helper.rb
+++ app/helpers/select2js_helper.rb
@@ -1,5 +1,20 @@
 module Select2jsHelper
+  # rubocop: disable Metrics/MethodLength
   def select2js_for_id(id)
-    javascript_tag("(function() { $('##{id}').select2({ width: '100%', theme: 'bootstrap' }); })();")
+    js = <<-JS
+    (function() {
+      $(document).on('turbolinks:before-cache', function() {
+        if($('##{id}').data('select2') !== undefined) {
+          $('##{id}').select2('destroy');
+        };
+      });
+      if($('##{id}').data('select2') !== undefined) {
+        return;
+      };
+      $('##{id}').select2({ width: '100%', theme: 'bootstrap' });
+    })();
+    JS
+    javascript_tag(js)
   end
+  # rubocop: enable Metrics/MethodLength
 end
