75ee0d87d02d17eb06a95872155bf692c6600a63
Oleg Antonyan <oleg.b.antonyan@gmail.com>
2016-06-29 16:52:16 +0200

    disable rack multipart workaround

---
+++ app/jobs/tmp_uploads_cleanup_job.rb
@@ -0,0 +1,7 @@
+class TmpUploadsCleanupJob < ApplicationJob
+  def perform
+    Dir.glob("#{Rails.root}/public/uploads/tmp/**/*").reject { |f| File.directory?(f) }.each do |f|
+      File.delete(f) if (Time.current - File.mtime(f)) > 604_800 # 7 days
+    end
+  end
+end
