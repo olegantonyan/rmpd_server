75ff785b26e9540c7054161ed11d06608bb0b7a1
Oleg Antonyan <oleg.b.antonyan@gmail.com>
2016-06-30 21:04:26 +0200

    fix SshTunnelNotifierJob call

--- app/services/deviceapi/protocol/incoming/ack_ok.rb
+++ app/services/deviceapi/protocol/incoming/ack_ok.rb
@@ -4,9 +4,8 @@ class Deviceapi::Protocol::Incoming::AckOk < Deviceapi::Protocol::Incoming::Base
     original_message = mq.remove(sequence_number)
     case original_message&.message_type
     when 'request_ssh_tunnel'
-      SshTunnelNotifierJob.perform_later
+      SshTunnelNotifierJob.perform_later(device)
     end
-    original_message
   end
   # rubocop: enable Lint/UnusedMethodArgument
 end
