3f03c112228893980fe22a414e1f433af4ae74dc
Oleg Antonyan <oleg.b.antonyan@gmail.com>
2016-07-23 20:54:06 +0200

    hide_navbar minor refactor

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -3,6 +3,10 @@ module ApplicationHelper
     content_for :title, page_title.to_s
   end
 
+  def hide_navbar?
+    params[:invitation_token].present? # hide navbar for those idiots who can't register via invite
+  end
+
   def app_title
     return 'use load_config to initialize APP_CONFIG' unless defined? APP_CONFIG
     APP_CONFIG[:app_title] || 'set app_title in config/config.yml'
