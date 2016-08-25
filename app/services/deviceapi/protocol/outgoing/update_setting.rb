4f4f519c06b0985725dfe706c898ca0e886aefde
Oleg Antonyan <oleg.b.antonyan@gmail.com>
2016-08-25 15:04:55 +0200

    add timezone name in update_setting command

--- app/services/deviceapi/protocol/outgoing/update_setting.rb
+++ app/services/deviceapi/protocol/outgoing/update_setting.rb
@@ -8,8 +8,11 @@ class Deviceapi::Protocol::Outgoing::UpdateSetting < Deviceapi::Protocol::Outgoi
   private
 
   def json(options = {})
-    result = {}
-    result[:time_zone] = device.time_zone_formatted_offset if options.fetch(:changed_attrs, []).include?(:time_zone)
-    result
+    {}.tap do |j|
+      if options.fetch(:changed_attrs, []).include?(:time_zone)
+        j[:time_zone] = device.time_zone_formatted_offset
+        j[:time_zone_name] = device.time_zone
+      end
+    end
   end
 end
