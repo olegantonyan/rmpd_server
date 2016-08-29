0e53dca660cb9ce0478543b988449f63178c0f6a
Oleg Antonyan <oleg.b.antonyan@gmail.com>
2016-08-29 12:15:51 +0200

    validate show duration less than 86400

--- app/models/playlist/item.rb
+++ app/models/playlist/item.rb
@@ -10,7 +10,7 @@ class Playlist::Item < ApplicationRecord
   with_options presence: true do
     validates :media_item
     validates :playlist
-    validates :show_duration, numericality: { greater_than_or_equal_to: 1 }, if: 'image?'
+    validates :show_duration, numericality: { greater_than_or_equal_to: 1, less_than: 86_400 }, if: 'image?'
   end
   validate :check_files_processing
   validate :begin_date_less_than_end_date
