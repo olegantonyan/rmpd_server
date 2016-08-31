6d93d10fe905116481cd0121fe569bf26a80bbb5
Oleg Antonyan <oleg.b.antonyan@gmail.com>
2016-08-31 19:32:14 +0200

    fix nil error on media itemsnavbar processing

--- app/helpers/media_items_helper.rb
+++ app/helpers/media_items_helper.rb
@@ -1,6 +1,6 @@
 module MediaItemsHelper
   def show_file_processing_navbar?
-    policy_scope(MediaItem.processing).exists?
+    user_signed_in? && policy_scope(MediaItem.processing).exists?
   end
 
   def file_processing_navbar_text
