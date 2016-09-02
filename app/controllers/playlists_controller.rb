a3257780977dbcb842dd82057d0d965487c2f8d3
Oleg Antonyan <oleg.b.antonyan@gmail.com>
2016-09-02 08:52:28 +0200

    fix nil error in playlists

--- app/controllers/playlists_controller.rb
+++ app/controllers/playlists_controller.rb
@@ -2,7 +2,7 @@ class PlaylistsController < BaseController
   include Filterrificable
 
   before_action :set_playlist, only: %i(show edit update destroy)
-  before_action :set_media_items, only: %i(new edit)
+  before_action :set_media_items, only: %i(new edit update create)
 
   # GET /playlists
   # rubocop: disable Metrics/AbcSize, Style/Semicolon, Style/RedundantParentheses
