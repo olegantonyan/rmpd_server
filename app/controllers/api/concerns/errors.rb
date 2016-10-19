9b6d533e9db7a7173d91a804b97c6e2ce73cbe44
Oleg Antonyan <oleg.b.antonyan@gmail.com>
2016-10-19 09:35:08 +0200

    prettyfy jwt auth

--- app/controllers/api/concerns/errors.rb
+++ app/controllers/api/concerns/errors.rb
@@ -5,23 +5,23 @@ module Api::Concerns::Errors
 
   included do
     rescue_from 'Exception' do |exception|
-      json_error(exception.message, 500, exception)
+      json_error(exception, 500)
     end
 
     rescue_from 'UnauthorizedError' do |exception|
-      json_error('unauthorized', 401, exception)
+      json_error(exception, 401)
     end
   end
 
   def unauthorized!
-    raise UnauthorizedError
+    raise UnauthorizedError, 'unauthorized'
   end
 
   private
 
-  def json_error(text, status, exception)
-    data = { error: text }
-    data[:stacktrace] = exception&.backtrace&.first(20) if Rails.env.development? || Rails.env.test?
+  def json_error(exception, status)
+    data = { error: exception.message }
+    data[:stacktrace] = exception.backtrace.first(20) if Rails.env.development? || Rails.env.test?
     render json: data, status: status
   end
 end
