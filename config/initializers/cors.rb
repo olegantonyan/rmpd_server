ba9948d8f4d3e50d01aca26003d004614164e1f5
Oleg Antonyan <oleg.b.antonyan@gmail.com>
2016-12-05 10:00:19 +0200

    cors production

--- config/initializers/cors.rb
+++ config/initializers/cors.rb
@@ -5,4 +5,10 @@ Rails.application.config.middleware.insert_before(0, Rack::Cors) do
       headers: :any,
       methods: %i(get post put patch delete options head)
   end
+  allow do
+    origins 'cloud.slon-ds.ru'
+    resource '*',
+      headers: :any,
+      methods: %i(get post put patch delete options head)
+  end
 end
