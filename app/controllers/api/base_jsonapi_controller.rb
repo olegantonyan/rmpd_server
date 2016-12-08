eb7978602d6d105021e22c0dc0112436bb4d52e7
Oleg Antonyan <oleg.b.antonyan@gmail.com>
2016-12-08 06:35:16 +0200

    dont include index acl

--- app/controllers/api/base_jsonapi_controller.rb
+++ app/controllers/api/base_jsonapi_controller.rb
@@ -16,7 +16,7 @@ class Api::BaseJsonapiController < Api::BaseController
   def base_meta
     model_class = JSONAPI::Resource.resource_for(params[:controller])._model_class
     policy = Pundit.policy!(context[:user], model_class)
-    acl = %i(create index).each_with_object({}) do |e, a|
+    acl = %i(create).each_with_object({}) do |e, a|
       a[e] = policy.public_send("#{e}?")
     end
     super.merge(acl: acl)
