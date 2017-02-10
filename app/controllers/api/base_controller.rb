e3dc03b307424829d2ab28b60b73717aeb56861d
Oleg Antonyan <oleg.b.antonyan@gmail.com>
2017-02-10 05:58:20 +0200

    disable 304 caching in development

--- app/controllers/api/base_controller.rb
+++ app/controllers/api/base_controller.rb
@@ -3,4 +3,11 @@ class Api::BaseController < ActionController::API
   include Api::Concerns::Authenticatable
 
   before_action :set_paper_trail_whodunnit
+  after_action :disable_caching, if: 'Rails.env.development?'
+
+  private
+
+  def disable_caching
+    headers['Last-Modified'] = Time.zone.now.httpdate
+  end
 end
