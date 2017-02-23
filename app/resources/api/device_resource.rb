f56a76d45db6cc9c0caa443b10aaffafad35b111
Oleg Antonyan <oleg.b.antonyan@gmail.com>
2017-02-23 09:06:15 +0200

    device status embed to device resource

--- app/resources/api/device_resource.rb
+++ app/resources/api/device_resource.rb
@@ -1,11 +1,21 @@
 class Api::DeviceResource < Api::BaseResource
   attributes :login, :name, :time_zone, :message_queue_sync_period, :wallpaper
 
+  def self.status_attributes
+    %i(online poweredon_at now_playing devicetime free_space)
+  end
+
+  attributes(*status_attributes)
+
   has_one :company
   has_one :playlist
   has_many :device_groups, class_name: 'DeviceGroup'
 
   def self.updatable_fields(context)
-    super - %i(login)
+    super - %i(login) - status_attributes
+  end
+
+  status_attributes.each do |a|
+    define_method(a) { model.device_status.public_send(a) }
   end
 end
