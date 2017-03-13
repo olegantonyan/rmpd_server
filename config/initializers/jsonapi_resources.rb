50d7858f33caf26b2242e361f71824d9347348fc
Oleg Antonyan <oleg.b.antonyan@gmail.com>
2017-03-13 09:04:43 +0200

    500 max page size

--- config/initializers/jsonapi_resources.rb
+++ config/initializers/jsonapi_resources.rb
@@ -3,7 +3,7 @@ JSONAPI.configure do |config|
   config.default_paginator = :paged
 
   config.default_page_size = 25
-  config.maximum_page_size = 100
+  config.maximum_page_size = 500
 
   config.top_level_meta_include_page_count = true
   config.top_level_meta_page_count_key = :page_count
