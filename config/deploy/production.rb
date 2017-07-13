71a953e0b28622258b3c791c52956f4f1b673b6d
Oleg Antonyan <oleg.b.antonyan@gmail.com>
2017-07-13 21:11:53 +0200

    deploy back to prod

--- config/deploy/production.rb
+++ config/deploy/production.rb
@@ -7,7 +7,7 @@
 # server 'example.com', user: 'deploy', roles: %w{app web}, other_property: :other_value
 # server 'db.example.com', user: 'deploy', roles: %w{db}
 
-server 'server.slon-ds.ru', user: 'badmotherfucker', roles: %w{app db web}
+server 'server.slon-ds.ru:10022', user: 'badmotherfucker', roles: %w{app db web}
 
 # role-based syntax
 # ==================
