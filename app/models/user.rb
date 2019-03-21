ddc9f1e182e31b1572f679e6dd23004677947606
Oleg Antonyan <oleg.b.antonyan@gmail.com>
2019-03-21 12:28:21 +0200

    disable devise async

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,5 +1,5 @@
 class User < ApplicationRecord
-  devise :database_authenticatable, :registerable, :recoverable, :rememberable, :trackable, :validatable, :confirmable, :async
+  devise :database_authenticatable, :registerable, :recoverable, :rememberable, :trackable, :validatable, :confirmable
 
   has_many :user_company_memberships, inverse_of: :user, dependent: :destroy
   has_many :invites, inverse_of: :user, dependent: :destroy
