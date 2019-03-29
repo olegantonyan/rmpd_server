f6e6770d3da630935cd4f4ecc7f9077a74a9af45
Oleg Antonyan <oleg.b.antonyan@gmail.com>
2019-03-29 12:17:31 +0200

    up to 64 puma threads

--- config/puma/production.rb
+++ config/puma/production.rb
@@ -1,6 +1,6 @@
 FileUtils.mkdir_p 'tmp/sockets'
 
-threads 4,16
+threads 4,64
 stdout_redirect 'log/puma_stdout.log', 'log/puma_stderr.log', true
 bind 'unix://tmp/sockets/puma.socket'
 environment 'production'
