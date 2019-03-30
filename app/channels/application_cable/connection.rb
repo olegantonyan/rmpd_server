0809fff4e61f2c4457c5361997d62c3d1ec50e08
Oleg Antonyan <oleg.b.antonyan@gmail.com>
2019-03-30 14:49:56 +0200

    cleanup

--- app/channels/application_cable/connection.rb
+++ app/channels/application_cable/connection.rb
@@ -8,14 +8,8 @@ module ApplicationCable
 
     private
 
-    def find_verified_user # rubocop: disable Metrics/AbcSize it's fine here
-      # @see config/initializers/warden_hooks.rb
-      verified_user = User.find_by(id: cookies.encrypted['user.id'])
-      if verified_user && cookies.encrypted['user.expires_at'] && Time.parse(cookies.encrypted['user.expires_at']).utc > Time.now.utc
-        verified_user
-      else
-        reject_unauthorized_connection
-      end
+    def find_verified_user
+      env['warden'].user || reject_unauthorized_connection # assuming Devise auth
     end
   end
 end
